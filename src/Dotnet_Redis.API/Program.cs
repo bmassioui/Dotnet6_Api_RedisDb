0badd58bae7951d81a5bd24d56b11530cc35e2a6
bmassioui <bouchaib.massioui@gmail.com>
2022-03-01 11:49:45 +0000

    remove unnecessary dependencies

--- src/Dotnet_Redis.API/Program.cs
+++ src/Dotnet_Redis.API/Program.cs
@@ -1,6 +1,9 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IConnectionMultiplexer>(options => ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("RedisDbConnectionString")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
